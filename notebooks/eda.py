ab8741947a7d1717b61c38b179b4776cedc04619
Renato Gregório <renatogregorio@protonmail.com>
2026-06-17 15:26:53 -0300

    feat: implement bronze layer

---
+++ notebooks/eda.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+df = pd.read_csv(
+    "data/raw/imdb_top_movies_1980_2026.csv"
+)
+
+print("\n===== INFO =====")
+df.info()
+
+print("\n===== NULOS =====")
+print(df.isnull().sum())
+
+print("\n===== DUPLICADOS =====")
+print(
+    df.duplicated(
+        subset=["imdb_id"]
+    ).sum()
+)
+
+print("\n===== AMOSTRA =====")
+print(df.head())
---
+++ src/transform/bronze.py
@@ -0,0 +1,69 @@
+from datetime import datetime, timezone
+from pathlib import Path
+
+import pandas as pd
+
+from src.utils.logger import logger
+
+RAW_FILE = Path(
+    "data/raw/imdb_top_movies_1980_2026.csv"
+)
+
+BRONZE_FILE = Path(
+    "data/bronze/movies_raw.parquet"
+)
+
+
+def create_bronze_layer() -> None:
+    try:
+        logger.info(
+            "[BRONZE] Iniciando geração da camada Bronze"
+        )
+
+        if not RAW_FILE.exists():
+            raise FileNotFoundError(
+                f"Arquivo não encontrado: {RAW_FILE}"
+            )
+
+        logger.info(
+            "[BRONZE] Lendo arquivo CSV"
+        )
+
+        df = pd.read_csv(RAW_FILE)
+
+        logger.info(
+            "[BRONZE] {} registros carregados",
+            len(df)
+        )
+
+        # Metadados técnicos
+        df["_ingestion_timestamp"] = datetime.now(
+            timezone.utc
+        )
+
+        df["_source"] = "kaggle"
+
+        BRONZE_FILE.parent.mkdir(
+            parents=True,
+            exist_ok=True
+        )
+
+        logger.info(
+            "[BRONZE] Salvando arquivo Parquet"
+        )
+
+        df.to_parquet(
+            BRONZE_FILE,
+            index=False
+        )
+
+        logger.success(
+            "[BRONZE] Camada Bronze criada com sucesso"
+        )
+
+    except Exception:
+        logger.exception(
+            "[BRONZE] Erro ao gerar camada Bronze"
+        )
+        raise
+    
